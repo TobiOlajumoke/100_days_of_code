9ca339d1c92d764068b2b862a402db291818a429
Tobi <olajumoketobi01@gmail.com>
2023-02-10 23:51:13 +0100

    update day 5

--- day_5/task.py
+++ day_5/task.py
@@ -1,2 +1,8 @@
-print("Welcome to day 5")
-print(input("what is your name?\n"))
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# print(student_heights)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
